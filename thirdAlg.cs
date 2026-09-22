abb4563b61addd97b0f5f6e2b12bc45670f7317b
lilcoder <231600759+nataha6824-coder@users.noreply.github.com>
2026-09-22 23:50:49 +0500

    Add files via upload

---
+++ thirdAlg.cs
@@ -0,0 +1,14 @@
+﻿public static class ProductFunction
+{
+    // сложность - O(n)
+    // рекомендуемый - 30_000_000
+    public static void Execute(double[] vector)
+    {
+        double product = 1.0;
+        for (int i = 0; i < vector.Length; i++)
+        {
+            product *= vector[i];
+        }
+        System.GC.KeepAlive(product);
+    }
+}
