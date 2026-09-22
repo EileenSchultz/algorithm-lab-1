670f6336ddfcdc55b77ea912c3b94425ca7af9b7
lilcoder <231600759+nataha6824-coder@users.noreply.github.com>
2026-09-22 23:43:41 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿public static class ConstantFunction
+{
+    // сложность алгоритма -  O(1)
+    // рекомендуемый максимум 50_000_000
+    public static void Execute(double[] vector)
+    {
+        double result = 1.0;
+        // защита от оптимизации компилятора
+        System.GC.KeepAlive(result);
+    }
+}
